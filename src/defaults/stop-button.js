331a6da5f2f4b8a2b328b0b56d67a608773aa6b2
Mike Cavaliere <mcavaliere@echobind.com>
2020-09-18 13:09:03 -0400

    Cleanup

--- src/defaults/stop-button.js
+++ src/defaults/stop-button.js
@@ -2,7 +2,6 @@ import React from 'react'
 import styled from 'styled-components'
 
 const Button = styled.button`
-  /* background: ${props => props.backgroundColor}; */
   background: pink;
   color: ${props => props.color};
   border-radius: 4px;
